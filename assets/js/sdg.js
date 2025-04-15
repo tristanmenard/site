444b271fc00d2f40646a8e1f1ca5b009ef6118b1
tristanmenard <54912651+tristanmenard@users.noreply.github.com>
2025-04-15 18:37:21 +0000

    Deploying to gh-pages from  @ 0f61a5dc7260bcfc32b9481ba4fc5bc61032e9c5 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3313,8 +3313,8 @@ var mapView = function () {
     $('.map').show();
     $('#map').sdgMap({
       indicatorId: indicatorId,
-      mapOptions: {"disaggregation_controls":false,"minZoom":5,"maxZoom":10,"tileURL":"","tileOptions":{"id":"","accessToken":"","attribution":""},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":""}},
-      mapLayers: [],
+      mapOptions: {"disaggregation_controls":false,"minZoom":2,"maxZoom":6,"tileURL":"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png","tileOptions":{"id":"","accessToken":"","attribution":"OpenStreetMap"},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":"5,5"}},
+      mapLayers: [{"subfolder":"regions","label":"Regions","min_zoom":2,"max_zoom":6}],
       precision: precision,
       precisionItems: precisionItems,
       decimalSeparator: decimalSeparator,
