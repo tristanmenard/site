f6004ea0aca6735c7f7c97dd84eefee2e44a991e
tristanmenard <54912651+tristanmenard@users.noreply.github.com>
2025-04-15 20:14:20 +0000

    Deploying to gh-pages from  @ adfe55fca6e70a285b816c8cf6e04cba76693012 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3313,7 +3313,7 @@ var mapView = function () {
     $('.map').show();
     $('#map').sdgMap({
       indicatorId: indicatorId,
-      mapOptions: {"disaggregation_controls":false,"minZoom":2,"maxZoom":6,"tileURL":"https://tile.openstreetmap.org/{z}/{x}/{y}.png","tileOptions":{"id":"","accessToken":"","attribution":"<a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a>"},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":"5,5"}},
+      mapOptions: {"disaggregation_controls":true,"minZoom":2,"maxZoom":6,"tileURL":"https://tile.openstreetmap.org/{z}/{x}/{y}.png","tileOptions":{"id":"","accessToken":"","attribution":"<a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a>"},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#deebf7","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#3182bd","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":"5,5"}},
       mapLayers: [{"subfolder":"regions","label":"Regions","min_zoom":2,"max_zoom":6}],
       precision: precision,
       precisionItems: precisionItems,
@@ -6161,7 +6161,7 @@ $(function() {
             this.needsMapUpdate = false;
             this.seriesColumn = 'Series';
             this.unitsColumn = 'Units';
-            this.displayForm = false;
+            this.displayForm = true;
             this.updateDisaggregations(plugin.startValues);
         },
 
